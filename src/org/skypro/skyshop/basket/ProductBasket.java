afc2c68b777addd4458a18223e1e5c65af3ab696
asus2 <izmailoff.ehduard@yandex.ru>
2025-03-07 15:29:06 +0300

    *****

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -1,4 +1,63 @@
 package org.skypro.skyshop.basket;
 
+import org.skypro.skyshop.product.Product;
+
 public class ProductBasket {
+    private final Product[] products;
+    private int count;
+
+    public ProductBasket() {
+        this.products = new Product[5];
+        this.count = 0;
+    }
+
+    public void addProduct(Product product) {
+        if (count < products.length) {
+            products[count++] = product;
+        } else {
+            System.out.println("Невозможно добавить продукт");
+        }
+    }
+
+    public int getTotalCost() {
+        int totalCost = 0;
+        for (int i = 0; i < count; i++) {
+            totalCost += products[i].getPrice();
+        }
+        return totalCost;
+    }
+
+    public void printBasket() {
+        if (count == 0) {
+            System.out.println("В корзине пусто");
+            return;
+        }
+
+        int specialCount = 0;
+        for (int i = 0; i < count; i++) {
+            System.out.println(products[i].toString());
+            if (products[i].isSpecial()) {
+                specialCount++;
+            }
+        }
+
+        System.out.println("Итого: " + getTotalCost());
+        System.out.println("Специальных товаров: " + specialCount);
+    }
+
+    public boolean containsProduct(String name) {
+        for (int i = 0; i < count; i++) {
+            if (products[i].getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clearBasket() {
+        for (int i = 0; i < count; i++) {
+            products[i] = null;
+        }
+        count = 0;
+    }
 }
