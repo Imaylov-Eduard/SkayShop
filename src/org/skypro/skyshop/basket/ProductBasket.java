c0de9e786b6c79ba72c9dae6a5713a5fac352a0e
asus2 <izmailoff.ehduard@yandex.ru>
2025-03-07 16:35:23 +0300

    *********

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -1,4 +1,59 @@
 package org.skypro.skyshop.basket;
 
+import org.skypro.skyshop.product.Product;
+
+import java.util.*;
+
 public class ProductBasket {
+    private final Map<String, List<Product>> productsMap;
+
+    public ProductBasket() {
+        this.productsMap = new HashMap<>();
+    }
+
+    public void addProduct(Product product) {
+        productsMap.computeIfAbsent(product.getName(), k -> new ArrayList<>()).add(product);
+    }
+
+    public List<Product> removeProductByName(String name) {
+        return productsMap.remove(name);
+    }
+
+    public int getTotalCost() {
+        int totalCost = 0;
+        for (List<Product> productList : productsMap.values()) {
+            for (Product product : productList) {
+                totalCost += product.getPrice();
+            }
+        }
+        return totalCost;
+    }
+
+    public void printBasket() {
+        if (productsMap.isEmpty()) {
+            System.out.println("В корзине пусто");
+            return;
+        }
+
+        int specialCount = 0;
+        for (List<Product> productList : productsMap.values()) {
+            for (Product product : productList) {
+                System.out.println(product.toString());
+                if (product.isSpecial()) {
+                    specialCount++;
+                }
+            }
+        }
+
+        System.out.println("Итого: " + getTotalCost());
+        System.out.println("Специальных товаров: " + specialCount);
+    }
+
+    public boolean containsProduct(String name) {
+        return productsMap.containsKey(name);
+    }
+
+    public void clearBasket() {
+        productsMap.clear();
+    }
 }
