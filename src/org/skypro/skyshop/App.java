7d1ddfa2f0d7308673f332b4bd1477fed4f9c691
asus2 <izmailoff.ehduard@yandex.ru>
2025-03-07 14:56:35 +0300

    ****

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,4 +1,43 @@
 package org.skypro.skyshop;
 
+import org.skypro.skyshop.article.Article;
+import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.product.DiscountedProduct;
+import org.skypro.skyshop.product.FixPriceProduct;
+import org.skypro.skyshop.product.SimpleProduct;
+import org.skypro.skyshop.search.SearchEngine;
+import org.skypro.skyshop.search.Searchable;
+
+import java.util.Arrays;
+
 public class App {
+    public static void main(String[] args) {
+        ProductBasket basket = new ProductBasket();
+
+        SimpleProduct laptop = new SimpleProduct("Ноутбук", 50000);
+        DiscountedProduct smartphone = new DiscountedProduct("Смартфон", 30000, 10);
+        FixPriceProduct headphones = new FixPriceProduct("Наушники");
+
+        basket.addProduct(laptop);
+        basket.addProduct(smartphone);
+        basket.addProduct(headphones);
+
+        Article article1 = new Article("Обзор ноутбуков", "Ноутбуки стали мощнее и доступнее.");
+        Article article2 = new Article("Новые смартфоны", "Смартфоны с улучшенными камерами.");
+
+        SearchEngine searchEngine = new SearchEngine(10);
+        searchEngine.add(laptop);
+        searchEngine.add(smartphone);
+        searchEngine.add(headphones);
+        searchEngine.add(article1);
+        searchEngine.add(article2);
+
+        System.out.println("Результаты поиска по запросу 'ноутбук':");
+        Searchable[] results = searchEngine.search("ноутбук");
+        System.out.println(Arrays.toString(results));
+
+        System.out.println("Результаты поиска по запросу 'смартфон':");
+        results = searchEngine.search("смартфон");
+        System.out.println(Arrays.toString(results));
+    }
 }
